50682c5f126a6c85c36f0338aad2c895a08d214c
aneja-arun1 <aneja.arun1@gmail.com>
2026-01-28 00:37:03 +0100

    Spacing between functions.

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "In Root"}
+
 @app.get("/items/{item_id}")
 async def get_items_by_item(item_id: int):
     return {"item_id": item_id}
