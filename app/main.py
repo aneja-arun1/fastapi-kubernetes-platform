523e4fc8ac5f15fd8d757c439328bc689f87122a
aneja-arun1 <aneja.arun1@gmail.com>
2026-01-28 03:34:11 +0100

    Adding function for getting item by query param.

--- app/main.py
+++ app/main.py
@@ -16,4 +16,19 @@ async def get_item_by_item_id(item_id: int):
             return_json = {"name": item["name"]}
     return return_json
 
+# E.g. https://localhost:8000/item?id=1
+@app.get("/item")
+async def get_item(id=None):
+    list_to_debug = [item["item_id"] for item in items]
+    print(list_to_debug)
+    return_json = {"name": ""}
+    for item in items:
+        if item["item_id"] == int(id):
+            return_json = {"name": item["name"]}
+            break
+        else:
+            return_json = {"name": "not found"}
+    return return_json
+
+
 
