f88b18c64af0bc13cad04e002c88a8ee02312553
aneja-arun1 <aneja.arun1@gmail.com>
2026-01-28 01:19:23 +0100

    Adding logic for get item.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 app = FastAPI()
+items = [
+    {"item_id": 1, "name": "orange"},
+    {"item_id": 2, "name": "apple"}, ]
+
 @app.get("/")
 async def root():
     return {"message": "In Root"}
 
 @app.get("/items/{item_id}")
-async def get_items_by_item(item_id: int):
-    return {"item_id": item_id}
+async def get_item_by_item_id(item_id: int):
+    return_json = {"name": ""}
+    for item in items:
+        if item["item_id"] == item_id:
+            return_json = {"name": item["name"]}
+    return return_json
+
 
